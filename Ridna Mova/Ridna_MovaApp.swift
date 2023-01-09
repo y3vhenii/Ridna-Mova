900dfd760c651d3531896d3c8ec7c80ecd641565
Yevhenii Ganusich <eganusich@gmail.com>
2023-01-09 13:18:39 -0500

    Initial Commit

---
+++ Ridna Mova/Ridna_MovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ridna_MovaApp.swift
+//  Ridna Mova
+//
+//  Created by Yevhenii Ganusich on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Ridna_MovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
