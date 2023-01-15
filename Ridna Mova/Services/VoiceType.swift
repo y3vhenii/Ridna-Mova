2abfadda48fbefb9063703405e07a492d9820dd2
Yevhenii Ganusich <eganusich@gmail.com>
2023-01-15 17:10:24 -0500

    Voice type is added for TextToVoice API service

---
+++ Ridna Mova/Services/VoiceType.swift
@@ -0,0 +1,14 @@
+//
+//  VoiceType.swift
+//  Ridna Mova
+//  Voice types used for TextToVoice service
+//  Created by Yevhenii Ganusich on 1/15/23.
+//
+
+import Foundation
+
+enum VoiceType: String {
+    case undefined
+    case standardFemaleUkr = "uk-UA-Standard-A"
+    case standardFemaleEng = "en-US-Standard-E"
+}
