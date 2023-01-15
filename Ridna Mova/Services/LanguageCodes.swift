afb266b8d086bc05ff52bba0267aefc5437d7652
Yevhenii Ganusich <eganusich@gmail.com>
2023-01-15 17:09:32 -0500

    Language code is added for TextToVoice service

---
+++ Ridna Mova/Services/LanguageCodes.swift
@@ -0,0 +1,14 @@
+//
+//  LanguageCodes.swift
+//  Ridna Mova
+//  Language codes used for TextToVoice service
+//  Created by Yevhenii Ganusich on 1/15/23.
+//
+
+import Foundation
+
+enum LanguageCode: String {
+    case undefined
+    case ukrUkrainian = "uk-UA"
+    case usEnglish = "en-US"
+}
